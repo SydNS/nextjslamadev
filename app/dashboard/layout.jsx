66e0bcdd48ce8898512f089f22a3aa3e193d65d3
SydNS <sydneynsiimbe@gmail.com>
2024-02-17 15:29:32 +0300

    call side and nav bars

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from "@/app/ui/dashboard/sidebar/sidebar";
+import Navbar from "@/app/ui/dashboard/navbar/navbar";
+
+const Layout = () =>{
+    return (
+        <div>
+
+            <div><Sidebar/></div>
+            <div><Navbar/></div>
+
+        </div>
+    )
+}
+
+export default Layout
