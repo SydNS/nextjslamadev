c5e7be62f932bb9ec1bdba9dab5bac76bb61ea99
SydNS <sydneynsiimbe@gmail.com>
2024-02-18 01:04:40 +0300

    adds footer to the dashboard layout

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -2,6 +2,7 @@ import React   from 'react'
 import Sidebar from "@/app/ui/dashboard/sidebar/sidebar";
 import Navbar  from "@/app/ui/dashboard/navbar/navbar";
 import styles  from "../ui/dashboard/dashboard.module.css"
+import Footer  from "@/app/ui/dashboard/footer/footer";
 
 const Layout = ({children}) => {
     return (
@@ -12,6 +13,7 @@ const Layout = ({children}) => {
             <div className={styles.content}>
                 <Navbar/>
                 {children}
+                <Footer/>
             </div>
         </div>
     )
