f7800fcdfa5d534a11fe9d9f34ba7154870f2113
SydNS <sydneynsiimbe@gmail.com>
2024-02-17 16:51:53 +0300

    creating sections in the list of the menu items

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,64 +1,79 @@
-import React                                                         from 'react'
-import {MdClass, MdDashboard, MdShop, MdShoppingBag, MdVerifiedUser} from "react-icons/md";
-import styles  from "./sidebar.module.css"
+import React                                                                              from 'react'
+import {MdClass, MdDashboard, MdPerson, MdPerson2, MdShop, MdShoppingBag, MdVerifiedUser} from "react-icons/md";
+import styles                                                                             from "./sidebar.module.css"
+import MenuLink
+                                                                     from "@/app/ui/dashboard/sidebar/menuLink/menuLink";
+import Image                                                                              from "next/image";
 
-const menuItems=[
+const menuItems = [
     {
-        title:"DashBoard",
-        list:[
+        title: "DashBoard",
+        list: [
             {
-                title:"Dashboard",
-                path:"/dashboard",
-                icon:<MdDashboard/>,
+                title: "Dashboard",
+                path: "/dashboard",
+                icon: <MdDashboard/>,
             },
             {
-                title:"Users",
-                path:"/users",
-                icon:<MdVerifiedUser/>,
+                title: "Users",
+                path: "/users",
+                icon: <MdVerifiedUser/>,
             }, {
                 title: "Markets",
                 path: "/markets",
-                icon:<MdShop/>,
+                icon: <MdShop/>,
             },
         ]
     },
     {
-        title:"Users",
-        list:[
+        title: "Users",
+        list: [
             {
-                title:"All",
-                path:"/dashboard",
-                icon:<MdDashboard/>,
+                title: "All",
+                path: "/dashboard",
+                icon: <MdPerson2/>,
             },
             {
-                title:"Add Users",
-                path:"/user",
-                icon:<MdDashboard/>,
+                title: "Add Users",
+                path: "/user",
+                icon: <MdPerson/>,
             }
         ]
     },
     {
-        title:"Market",
-        list:[
+        title: "Market",
+        list: [
             {
-                title:"markets",
-                path:"/markets",
-                icon:<MdShop/>,
+                title: "markets",
+                path: "/markets",
+                icon: <MdShop/>,
             },
             {
-                title:"Add Market",
-                path:"/market",
-                icon:<MdShoppingBag/>,
+                title: "Add Market",
+                path: "/market",
+                icon: <MdShoppingBag/>,
             }
         ]
     }
 ]
 const Sidebar = () => (
     <div className={styles.container}>
+        <div className={styles.user}>
+            <Image className={styles.userImage} src="/public/noavatar.png" alt="user image" width="50" height="50"/>
+            <span className={styles.username}>Sydney Doe</span>
+            <span className={styles.userTitle}>Sr Dev</span>
+        </div>
         <ul>
-            {menuItems.map((item)=>{
+            {menuItems.map((item) => {
                 return (
-                    <li key={item.title}>{item.title}</li>
+                    <li key={item.title}>
+                        <span className={styles.item}>{item.title}</span>
+                        <ul>
+                            {item.list.map(it=>(
+                                <MenuLink item={it} key={item.title}/>
+                            ))}
+                        </ul>
+                    </li>
                 )
             })}
         </ul>
