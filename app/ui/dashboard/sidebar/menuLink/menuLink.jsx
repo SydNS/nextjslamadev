c719b9d85ec1db0426eafee448f089c2196ef359
SydNS <sydneynsiimbe@gmail.com>
2024-02-17 17:35:08 +0300

    checks the current path and also adds use client since pathname used only on the client

--- app/ui/dashboard/sidebar/menuLink/menuLink.jsx
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -1,19 +1,29 @@
-import React  from 'react'
-import Link   from "next/link";
-import styles from "./menuLink.module.css"
+"use client"
+import React         from 'react'
+import Link          from "next/link";
+import styles        from "./menuLink.module.css"
+import {usePathname} from "next/navigation";
 
 // {
 // title:"Dashboard",
 // path:"/dashboard",
 // icon:<MdDashboard/>,
 // },
-const MenuLink = ({item}) => (
-    <Link href={item.path} className={styles.container}>
-        <p>
-            {item.icon}
-            {item.title}
-        </p>
-    </Link>
-);
+const MenuLink = ({item}) => {
+    const pathname = usePathname();
+    return (
+
+        <Link href={item.path} className={
+            `${styles.container} ${pathname === item.path && styles.active}`
+        }>
+            <i className={styles.linkIcon}>
+                {item.icon}
+            </i>
+            <p className={styles.linkText}>
+                {item.title}
+            </p>
+        </Link>
+    )
+};
 
 export default MenuLink
