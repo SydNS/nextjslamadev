cf3f70517a655a963ad682e098b1cdc5ada7c3ff
SydNS <sydneynsiimbe@gmail.com>
2024-02-18 01:04:21 +0300

    creates a footer for the dashboard

---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './footer.module.css'
+const Footer = () => (
+    <div className={styles.container}>
+        <div className={styles.logo}>Doers Solutions</div>
+        <div className={styles.text}>All rights reserved.</div>
+    </div>
+);
+
+export default Footer
