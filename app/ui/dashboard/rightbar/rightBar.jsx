2c2144b2a3522a551a919a72cd2ee6ab9f0ad010
SydNS <sydneynsiimbe@gmail.com>
2024-02-18 00:44:52 +0300

    adds details

--- app/ui/dashboard/rightbar/rightBar.jsx
+++ app/ui/dashboard/rightbar/rightBar.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import styles from './rightbar.module.css'
 import Image from "next/image";
-import {MdPlayCircleFilled} from "react-icons/md";
+import {MdPlayCircleFilled}                          from "react-icons/md";
 const RightBar = () => {
+
     return (
         <div className={styles.container}>
+            <div className={styles.item}>
+                <div className={styles.bgContainer}>
+                    <Image src="/astronaut.png" alt="picture" width="50" height="50"/>
+                </div>
+                <div className={styles.texts}>
+                    <span className={styles.notification}>Available Now
+                    </span>
+                    <h3 className={styles.title}>How to use the new version of admin dashboard
+                    </h3>
+                    <span className={styles.subtitle}>Takes 4 minutes to learn </span>
+                    <p className={styles.desc}>
+                        Lorem ipsum  dolor sit amet Lorem ipsum  dolor sit amet Lorem ipsum  dolor sit amet
+                        Lorem ipsum  dolor sit amet
+                    </p>
+                    <button className={styles.button}>
+                        <MdPlayCircleFilled/>
+                        Watch
+                    </button>
+                </div>
+            </div>
+
             <div className={styles.item}>
                 <div className={styles.bgContainer}>
                     <Image src="/astronaut.png" alt="picture" width="50" height="50"/>
