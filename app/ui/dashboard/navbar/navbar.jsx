af144c5529bda09ad34452b208c5899023aa74d7
SydNS <sydneynsiimbe@gmail.com>
2024-02-17 18:02:39 +0300

    creates a navbar ->breadCrumb ->menu and search on the right

--- app/ui/dashboard/navbar/navbar.jsx
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+'use client'
+import React                                                from 'react'
+import {usePathname}                                        from "next/navigation";
+import styles                                               from './navbar.module.css'
+import {MdNotifications, MdOutlineChat, MdPublic, MdSearch} from "react-icons/md";
 
-const Navbar = () => (
-    <div>Navbar</div>
-);
+const Navbar = () => {
+    const pathname = usePathname()
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>{pathname.split("/")}</div>
+            <div className={styles.menu}>
+                <div className={styles.search}>
+                    <MdSearch/>
+                    <input type="text" placeholder="Search..." className={styles.input}/>
+                </div>
+                <div className={styles.icons}>
+                    <MdOutlineChat size={20}/>
+                    <MdNotifications size={20}/>
+                    <MdPublic size={20}/>
+                </div>
+            </div>
+        </div>
+    )
+
+}
 
 export default Navbar
