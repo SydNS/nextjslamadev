022e2a1eb9201301cf14c08f78710b1d931a05da
SydNS <sydneynsiimbe@gmail.com>
2024-02-17 15:58:17 +0300

    adds style classes

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React   from 'react'
 import Sidebar from "@/app/ui/dashboard/sidebar/sidebar";
-import Navbar from "@/app/ui/dashboard/navbar/navbar";
-import styles from "../ui/dashboard/dashboard.module.css"
+import Navbar  from "@/app/ui/dashboard/navbar/navbar";
+import styles  from "../ui/dashboard/dashboard.module.css"
 
-const Layout = () =>{
+const Layout = ({children}) => {
     return (
-        <div>
-
-            <div><Sidebar/></div>
-            <div><Navbar/></div>
-
+        <div className={styles.container}>
+            <div className={styles.menu}>
+                <Sidebar/>
+            </div>
+            <div className={styles.content}>
+                <Navbar/>
+                {children}
+            </div>
         </div>
     )
 }
