52b2b9d72723a60386efaecee4b73a30fbc81a35
SydNS <sydneynsiimbe@gmail.com>
2024-02-17 19:52:55 +0300

    transaction.jsx to pick the transactions from a list

--- app/ui/dashboard/transactions/transaction.jsx
+++ app/ui/dashboard/transactions/transaction.jsx
@@ -1,54 +1,66 @@
-import React  from 'react'
+import React from 'react'
 import styles from "./transaction.module.css"
-import Image  from "next/image";
+import Image from "next/image";
 
-const Transaction = () => {
-    return (<div className={styles.container}>
-        <h2 className={styles.title}> Latest Transactions</h2>
-        <table className={styles.table}>
-            <thead>
-            <tr>
-                <td>Name</td>
-                <td>Status</td>
-                <td>Date</td>
-                <td>Amount</td>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>
-                    <div className={styles.user}>
-                        <Image src="/noavatar.png" alt="user image" width={40} height={40}
-                               className={styles.userImage}/>
-                        John Doe
-                    </div>
-                </td>
-                <td>
-                    <span className={`${styles.status} ${styles.pending}`}>
-                        Done
-                    </span>
-                </td>
-                <td>23rd/Jan/2023</td>
-                <td>6000000</td>
-            </tr>
-            <tr>
-                <td>
-                    <div className={styles.user}>
-                        <Image src="/astronaut.png" alt="user image" width={40} height={40}
-                               className={styles.userImage}/>
-                        John Doe
-                    </div>
-                </td>
-                <td>
-                    <span className={`${styles.status} ${styles.pending}`}>
-                        Pending
-                    </span>
-                </td>
-                <td>23rd/Jan/2023</td>
-                <td>6000000</td>
-            </tr>
-            </tbody>
-        </table>
-    </div>)
+export const transactions = [
+    {
+        name: 'Jane Doe',
+        status: 'Done',
+        date: '23rd/Jan/2023',
+        amount: 6000000,
+        image: '/noavatar.png'
+    },
+    {
+        name: 'John Doe',
+        status: 'Pending',
+        date: '23rd/Jan/2023',
+        amount: 6000000,
+        image: '/astronaut.png'
+    },
+    {
+        name: 'Steven Doe',
+        status: 'Pending',
+        date: '3rd/Feb/2023',
+        amount: 6000000,
+        image: '/noproduct.jpg'
+    }
+];
+const Transaction = ({ transactions }) => {
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.title}>Latest Transactions</h2>
+            <table className={styles.table}>
+                <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>Status</td>
+                    <td>Date</td>
+                    <td>Amount</td>
+                </tr>
+                </thead>
+                <tbody>
+                {transactions.map((transaction, index) => (
+                    <tr key={index}>
+                        <td>
+                            <div className={styles.user}>
+                                <Image src={transaction.image} alt="user image" width={40} height={40}
+                                       className={styles.userImage}/>
+                                <span className={styles.userName}>{transaction.name}</span>
+                            </div>
+                        </td>
+                        <td>
+                                <span className={`${styles.status} ${styles[transaction.status.toLowerCase()]}`}>
+                                    {transaction.status}
+                                </span>
+                        </td>
+                        <td>{transaction.date}</td>
+                        <td>{transaction.amount}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
+
 export default Transaction
