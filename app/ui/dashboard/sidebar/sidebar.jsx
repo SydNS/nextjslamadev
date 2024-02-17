fea3748f744195ab02b96dbc30738cdf47faeac2
SydNS <sydneynsiimbe@gmail.com>
2024-02-17 15:58:01 +0300

    adds a list of menu items for the navbar to loop

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,5 +1,26 @@
 import React from 'react'
 
+const menuItems=[
+    {
+        title:"Pages",
+        list:[
+            {
+                title:"Dashboard",
+                path:"/dashboard",
+                // icon:<MdDashboard/>,
+            },
+            {
+                title:"Users",
+                path:"/users",
+                // icon:<MdDashboard/>,
+            }, {
+                title: "Markets",
+                path: "/markets",
+                // icon:<MdDashboard/>,
+            },
+        ]
+    }
+]
 const Sidebar = () => (
     <div>Sidebar</div>
 );
