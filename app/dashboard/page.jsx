0f49a88caf0826fb3268e8d33c19b473946b5557
SydNS <sydneynsiimbe@gmail.com>
2024-02-17 19:53:09 +0300

    passes list in props

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Card  from "@/app/ui/dashboard/card/card";
 import styles from "../ui/dashboard/dashboard.module.css"
-import RightBar from "@/app/ui/dashboard/rightbar/rightBar";
-import Transaction from "@/app/ui/dashboard/transactions/transaction";
-import Chart from "@/app/ui/dashboard/chart/chart";
+import RightBar                    from "@/app/ui/dashboard/rightbar/rightBar";
+import Transaction, {transactions} from "@/app/ui/dashboard/transactions/transaction";
+import Chart                       from "@/app/ui/dashboard/chart/chart";
 const Dashboard = () => (
     <div className={styles.wrapper}>
         <div className={styles.main}>
@@ -12,7 +12,7 @@ const Dashboard = () => (
                 <Card/>
                 <Card/>
             </div>
-            <Transaction/>
+            <Transaction transactions={transactions}/>
             <Chart/>
         </div>
         <div className={styles.side}>
