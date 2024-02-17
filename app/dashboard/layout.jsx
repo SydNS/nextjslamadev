001cfed057e456ae1b26ac4e8ed6297a95a05a6a
SydNS <sydneynsiimbe@gmail.com>
2024-02-17 15:47:08 +0300

    imports these styles

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Sidebar from "@/app/ui/dashboard/sidebar/sidebar";
 import Navbar from "@/app/ui/dashboard/navbar/navbar";
+import styles from "../ui/dashboard/dashboard.module.css"
 
 const Layout = () =>{
     return (
