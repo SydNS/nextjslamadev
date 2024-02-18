88a71e8770572f640332b4e82bf28b2957e3c202
SydNS <sydneynsiimbe@gmail.com>
2024-02-18 11:01:55 +0300

    creates two components ->table ->tableTop

--- app/dashboard/users/page.js
+++ app/dashboard/users/page.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React  from 'react'
+import styles from './user.module.css'
+import Search from "@/app/ui/dashboard/search/search";
+import Link   from "next/link";
 
-const User = () => (
-    <div>User</div>
-);
+function TableTop() {
+    return <div className={styles.top}>
+        <Search placeholder="Search . . ."/>
+        <Link className={styles.addButton} href="/dashboard/users/add">
+            <button>Add User</button>
+        </Link>
+    </div>;
+}
 
+const User = () => {
+    return (
+        <div className={styles.container}>
+
+            <TableTop/>
+            <table className={styles.tables}>
+
+            </table>
+        </div>
+    )
+}
 export default User
