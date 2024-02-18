fbb7c9cdedecbf880cb9770742dc3eae32136cc4
SydNS <sydneynsiimbe@gmail.com>
2024-02-18 19:17:52 +0300

    adds more constants for the

---
+++ components/constants.js
@@ -0,0 +1,111 @@
+export const userData = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@example.com',
+        createdAt: '2024-02-18',
+        role: 'Admin',
+        status: 'Active',
+        image: '/noavatar.png'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'jane@example.com',
+        createdAt: '2024-02-17',
+        role: 'User',
+        status: 'Inactive',
+        image: '/astronaut.png'
+    },
+    {
+        id: 3,
+        name: 'Alice Smith',
+        email: 'alice@example.com',
+        createdAt: '2024-02-16',
+        role: 'User',
+        status: 'Active',
+        image: '/noavatar.png'
+    },
+    {
+        id: 4,
+        name: 'Bob Johnson',
+        email: 'bob@example.com',
+        createdAt: '2024-02-15',
+        role: 'Admin',
+        status: 'Inactive',
+        image: '/noavatar.png'
+    },
+    {
+        id: 5,
+        name: 'Alice Smith',
+        email: 'alice@example.com',
+        createdAt: '2024-02-16',
+        role: 'User',
+        status: 'Active',
+        image: '/noavatar.png'
+    },
+    {
+        id: 6,
+        name: 'Bob Johnson',
+        email: 'bob@example.com',
+        createdAt: '2024-02-15',
+        role: 'Admin',
+        status: 'Inactive',
+        image: '/noavatar.png'
+    }
+];
+
+export const marketData = [
+    {
+        id: 1,
+        name: 'Market 1',
+        location: 'Location 1',
+        createdAt: '2024-02-18',
+        role: 'Role 1',
+        numberOfWorkers: 10,
+        status: 'Active',
+        image: '/noavatar.png'
+    },
+    {
+        id: 2,
+        name: 'Market 2',
+        location: 'Location 2',
+        createdAt: '2024-02-17',
+        role: 'Role 2',
+        numberOfWorkers: 20,
+        status: 'Inactive',
+        image: '/astronaut.png'
+    },
+    {
+        id: 3,
+        name: 'Market 3',
+        location: 'Location 3',
+        createdAt: '2024-02-16',
+        role: 'Role 3',
+        numberOfWorkers: 15,
+        status: 'Active',
+        image: '/astronaut.png'
+    },
+    {
+        id: 4,
+        name: 'Market 4',
+        location: 'Location 4',
+        createdAt: '2024-02-15',
+        role: 'Role 4',
+        numberOfWorkers: 30,
+        status: 'Inactive',
+        image: '/astronaut.png'
+    },
+    {
+        id: 5,
+        name: 'Market 5',
+        location: 'Location 5',
+        createdAt: '2024-02-14',
+        role: 'Role 5',
+        numberOfWorkers: 25,
+        status: 'Active',
+        image: '/noavatar.png'
+    },
+    // Add more market objects as needed
+];
+
